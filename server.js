0668e76987ad834c3ee669ddb69300d1e6de05f7
Rob Hess <robwhess@gmail.com>
2019-06-05 10:00:47 -0700

    Add code from 6/5 lecture.

--- server.js
+++ server.js
@@ -3,11 +3,21 @@ var path = require('path');
 var express = require('express');
 var exphbs = require('express-handlebars');
 var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
 
 var peopleData = require('./peopleData');
 
 var app = express();
 var port = process.env.PORT || 3000;
+
+var mongoHost = process.env.MONGO_HOST;
+var mongoPort = process.env.MONGO_PORT || 27017;
+var mongoUser = process.env.MONGO_USER;
+var mongoPassword = process.env.MONGO_PASSWORD;
+var mongoDBName = process.env.MONGO_DB_NAME;
+
+var mongoUrl = `mongodb://${mongoUser}:${mongoPassword}@${mongoHost}:${mongoPort}/${mongoDBName}`;
+var db = null;
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
@@ -62,6 +72,12 @@ app.get('*', function (req, res, next) {
   res.status(404).render('404');
 });
 
-app.listen(port, function () {
-  console.log("== Server listening on port", port);
-})
+MongoClient.connect(mongoUrl, function (err, client) {
+  if (err) {
+    throw err;
+  }
+  db = client.db(mongoDBName);
+  app.listen(port, function () {
+    console.log("== Server listening on port", port);
+  });
+});
